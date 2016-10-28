12b6f0ca85dde4e4c1413e40b06637d87cf183ca
Arturo Guzman <arturo@guzart.com>
2016-10-28 15:11:25 -0400

    feat: use custom extensions and resolve root package

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # eslint-import-resolver-reactnative
+
+[![npm](https://img.shields.io/npm/v/eslint-import-resolver-node.svg)](https://www.npmjs.com/package/eslint-import-resolver-reactnative)
+
+React Native module resolution plugin for [`eslint-plugin-import`](https://www.npmjs.com/package/eslint-plugin-import).
+
+Uses [`eslint-import-resolver-node`](https://www.npmjs.com/package/eslint-import-resolver-node) with a few modifications:
+
+  * Configures extensions for React Native `['.js', '.json', '.android.js', '.ios.js']`
+  * Helps resolve project name paths to the root ([`eslint-plugin-import#626`](https://github.com/benmosher/eslint-plugin-import/issues/626))
+
---
+++ index.js
@@ -0,0 +1,39 @@
+var findRoot = require('find-root');
+var nodeResolver = require('eslint-import-resolver-node');
+var path = require('path');
+
+var log = require('debug')('eslint-plugin-import:resolver:reactnative');
+
+function pluginSettings(settings) {
+  return Object.assign(
+    {
+      extensions: ['.js', '.json', '.android.js', '.ios.js'],
+    },
+    settings
+  );
+}
+
+exports.interfaceVersion = 2;
+
+exports.resolve = function (source, file, userSettings) {
+  log('Resolving:', source, 'from:', file);
+  
+  var settings = pluginSettings(userSettings);
+  
+  try {
+    var appRoot = findRoot(file);
+    var package = require(path.join(appRoot, 'package.json'));
+    if (package.name) {
+      var namePath = package.name + '/';
+      if (source.indexOf(namePath) === 0) {
+        var absSource = path.join(appRoot, source.replace(namePath, ''));
+        return nodeResolver.resolve(absSource, file, settings);
+      }
+    }
+  } catch (err) {
+    log('Error:', err);
+    log('Fall back to eslint-import-resolver-node');
+  }
+
+  return nodeResolver.resolve(source, file, settings);
+};
