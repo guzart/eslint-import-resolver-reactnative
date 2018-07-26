8ce5a606bf0e6458cd44132e557b1ba4170d5825
Juan <juan@pleo.io>
2018-07-26 09:55:44 +0200

    adding TS support to eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var findRoot = require('find-root');
-var nodeResolver = require('eslint-import-resolver-node');
-var path = require('path');
+var findRoot = require("find-root");
+var nodeResolver = require("eslint-import-resolver-node");
+var path = require("path");
 
-var log = require('debug')('eslint-plugin-import:resolver:reactnative');
+var log = require("debug")("eslint-plugin-import:resolver:reactnative");
 
 function pluginSettings(settings) {
   return Object.assign(
     {
-      extensions: ['.js', '.json', '.android.js', '.ios.js'],
+      extensions: [".js", ".json", ".android.js", ".ios.js", ".ts", ".tsx"]
     },
     settings
   );
@@ -15,24 +15,24 @@ function pluginSettings(settings) {
 
 exports.interfaceVersion = 2;
 
-exports.resolve = function (source, file, userSettings) {
-  log('Resolving:', source, 'from:', file);
-  
+exports.resolve = function(source, file, userSettings) {
+  log("Resolving:", source, "from:", file);
+
   var settings = pluginSettings(userSettings);
-  
+
   try {
     var appRoot = findRoot(file);
-    var package = require(path.join(appRoot, 'package.json'));
+    var package = require(path.join(appRoot, "package.json"));
     if (package.name) {
-      var namePath = package.name + '/';
+      var namePath = package.name + "/";
       if (source.indexOf(namePath) === 0) {
-        var absSource = path.join(appRoot, source.replace(namePath, ''));
+        var absSource = path.join(appRoot, source.replace(namePath, ""));
         return nodeResolver.resolve(absSource, file, settings);
       }
     }
   } catch (err) {
-    log('Error:', err);
-    log('Fall back to eslint-import-resolver-node');
+    log("Error:", err);
+    log("Fall back to eslint-import-resolver-node");
   }
 
   return nodeResolver.resolve(source, file, settings);
